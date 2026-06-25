4eab5ac154b1492f6105251660a4aaa1679199f5
Heavyymir <richan005@outlook.com>
2026-06-25 15:42:25 +1000

    3: Created an interactive REPL loop in main to support the program. Uses CleanInput from commit 2 to clean user inputs

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"os"
+	"fmt"
+	)
 
 func main(){
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex >  ")
+		scanner.Scan()
+		words := cleanInput(scanner.Text())
+		fmt.Println("Your command was:", words[0])
 
-fmt.Println("Hello, World!")
-
+	}
 }
